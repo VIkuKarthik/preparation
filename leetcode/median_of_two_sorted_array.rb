30410508dc1337db0f38f433c584f8c75de3d831
ViKramaN <vikramank316@gmail.com>
2022-05-04 00:46:17 +0530

    median of two sorted array

--- leetcode/median_of_two_sorted_array.rb
+++ leetcode/median_of_two_sorted_array.rb
@@ -9,17 +9,46 @@
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @return {Float}
-def find_median_sorted_arrays(nums1, nums2)
-  merged_array = nums1 + nums2
-  length = merged_array.length
-  middle = length / 2
-  if length.even?
-    result = (merged_array[middle] + merged_array[middle -1]) / 2.0
-  else
-    result = merged_array[middle]
+def fin_median(nums1, nums2)
+  a, b = nums1, nums2
+  total = nums1.length + nums2.length
+  half = total / 2
+
+  if b.length < a.length
+    a, b = b, a
   end
-  result
+
+  l, r = 0, a.length - 1
+
+  while true do
+    i = (l + r) / 2 # A
+    j = (half - i) - 2 # B
+
+    aleft = (i >= 0) ? a[i] : -(Float::INFINITY)
+    aright = ( (i + 1) < (a.length) ) ? a[i + 1] : Float::INFINITY
+    bleft = (j >= 0) ? b[j] : -(Float::INFINITY)
+    bright = ( (j + 1) < (b.length) ) ? b[j + 1] : Float::INFINITY
+
+
+    # partition is correct
+    if aleft <= bright and bleft <= aright
+      
+      # odd
+      if (total % 2) != 0
+        return [aright, bright].min
+      end
+      # even
+      return ([aleft, bleft].max + [aright, bright].min) / 2
+    elsif aleft > bright
+      r = i - 1
+    else
+      l = i + 1
+    end
+  end
+
 end
 
-puts find_median_sorted_arrays([1,3], [2])
-puts find_median_sorted_arrays([1,2], [3,4])
+puts fin_median([1,2], [3,4])
+puts fin_median([1, 4, 5, 6, 7, 8, ], [7, 8, 9])
+puts fin_median([1,3], [2])
+puts fin_median([1,2], [3,4])
