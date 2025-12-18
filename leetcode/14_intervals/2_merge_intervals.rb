f04ab1393f0689e6b7e7b8453ebaec3d4df4cd3b
Vikraman <vikramank316@gmail.com>
2025-12-18 14:10:58 +0530

    INTERVALS: merge intervals

---
+++ leetcode/14_intervals/2_merge_intervals.rb
@@ -0,0 +1,47 @@
+=begin
+  Merge Intervals
+
+  Given an array of intervals where intervals[i] = [starti, endi],
+  merge all overlapping intervals, and return an array of the
+  non-overlapping intervals that cover all the intervals in the input.
+
+  Example 1:
+
+  Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+  Output: [[1,6],[8,10],[15,18]]
+  Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+
+  Example 2:
+
+  Input: intervals = [[1,4],[4,5]]
+  Output: [[1,5]]
+  Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+  Example 3:
+
+  Input: intervals = [[4,7],[1,4]]
+  Output: [[1,7]]
+  Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+=end
+
+def merge(intervals)
+  intervals.sort!
+  result = []
+
+  intervals.each do |interval|
+    if result.empty?
+      result << interval
+    elsif result[-1][1] >= interval[0]
+      end_max = [result[-1][1], interval[1]].max
+      result[-1][1] = end_max
+    else
+      result << interval
+    end
+  end
+  result
+end
+
+# intervals = [[1,4],[4,5]]
+# intervals = [[4,7],[1,4]]
+intervals = [[1,3],[2,6],[8,10],[15,18]] # [[1,6],[8,10],[15,18]]
+p merge(intervals)
